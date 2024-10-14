c06b64bcbc3ba8282896990dd4adeb6d5c1c7ee3
Nikita <kit1datasvit@gmail.com>
2024-10-14 15:44:08 +0300

    delete first and last charsters

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -2,5 +2,8 @@ package mate.academy
 
 fun removeChars(str: String): String {
     // Implement this function
-    return ""
+    if (str.length == 2) {
+        println("")
+    }
+    return str.drop(1).dropLast(1)
 }
